39d502dfc06454652cc53d6ed42ea279c91b6daf
Danilo Cavalcanti <danilcsouza22o@gmail.com>
2026-09-22 10:52:38 -0300

    feat: menu de livros

--- src/main.c
+++ src/main.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 #include "../include/livros.h"
-#include "../include/usuarios.h"
 
 int main(void) {
-    printf("Sistema de Biblioteca Universitaria\n");
+    Livro livros[MAX_LIVROS];
+    int totalLivros = 0;
+    int opcao;
+
+    do {
+        printf("\n=== SISTEMA DE BIBLIOTECA UNIVERSITARIA ===\n");
+        printf("1 - Cadastrar livro\n");
+        printf("2 - Listar livros\n");
+        printf("0 - Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+        limparBuffer();
+
+        switch (opcao) {
+            case 1:
+                cadastrarLivro(livros, &totalLivros);
+                break;
+
+            case 2:
+                listarLivros(livros, totalLivros);
+                break;
+
+            case 0:
+                printf("\nPrograma encerrado.\n");
+                break;
+
+            default:
+                printf("\nOpcao invalida.\n");
+        }
+
+    } while (opcao != 0);
 
     return 0;
 }
